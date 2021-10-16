909c21c8281489be0b2fcdf752c6098d555565a7
Pierre <bonnamy.77@gmail.com>
2021-10-16 15:35:28 +0200

    populate map

--- Malloc_World_-_Freeing_of_the_dedicated_ram/header.h
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/header.h
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
+enum values{
+    playerVal = 1,
+    portal2Val = -3,
+    portal1Val = -2,
+    wallVal = -1,
+    npcVal = 2,
+    plant1Val = 3,
+    rock1Val = 4,
+    wood1Val = 5,
+    plant2Val = 6,
+    rock2Val = 7,
+    wood2Val = 8,
+    plant3Val = 9,
+    rock3Val = 10,
+    wood3Val = 11,
+};
 
 void initMap ();
--- Malloc_World_-_Freeing_of_the_dedicated_ram/map.c
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/map.c
@@ -30,11 +30,37 @@ int ** createLevel(int rows, int columns){
     return map;
 }
 
+void populate(int **map, int rows, int columns, int entity, int max){
+    int row = rand2(0,rows-1);
+    int column = rand2(0,columns-1);
+
+    for(int i = 0 ; i< max; i++){
+        while(map[row][column] != 0){
+            row = rand2(0,rows-1);
+            column = rand2(0,rows-1);
+        }
+        map[row][column] = entity;
+    }
+}
+
+void populateLevel(int **map, int rows, int columns){
+    populate(map, rows, columns, playerVal, 1);
+    populate(map, rows, columns, plant1Val, 3);
+    populate(map, rows, columns, rock1Val, 3);
+    populate(map, rows, columns, wood1Val, 3);
+    populate(map, rows, columns, portal1Val, 1);
+    populate(map, rows, columns, npcVal, 1);
+    populate(map, rows, columns, wallVal, 7);
+}
+
 void initMap(){
-    int rows = 10;
-    int columns = 10;
+    srand(time(NULL));
+
+    int rows = 7;
+    int columns = 7;
 
     int **lv1 = createLevel(rows,columns);
+    populateLevel(lv1, rows, columns);
 
     printMap(lv1, rows, columns);
 }
