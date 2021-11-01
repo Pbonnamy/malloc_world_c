3ec6d68e279c10bf21b911c6f95b2d486cd36bc8
Pierre <bonnamy.77@gmail.com>
2021-11-01 17:59:25 +0100

    move function

--- Malloc_World_-_Freeing_of_the_dedicated_ram/movement.c
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/movement.c
@@ -1,16 +1,39 @@
 #include "header.h"
 
-void handleMovement(Levels *lv){
 
-    printMap(lv->lv1,lv->rows,lv->columns);
+void move (int ** level, PlayerPos * pc, char direction){
 
-    /*char direction;
+    level[pc->posRow][pc->posColumn] = 0;
+
+    if(direction == 'z'){
+        level[(pc->posRow)-1][pc->posColumn] = 1;
+        pc->posRow -= 1;
+    }else if(direction == 's'){
+        level[pc->posRow+1][pc->posColumn] = 1;
+        pc->posRow += 1;
+    }else if(direction == 'q'){
+        level[pc->posRow][pc->posColumn-1] = 1;
+        pc->posColumn -= 1;
+    }else if (direction == 'd'){
+        level[pc->posRow][pc->posColumn+1] = 1;
+        pc->posColumn += 1;
+    }
+
+}
+
+void handleMovement(Levels *lv, PlayerPos *pc){
+
+    //printMap(lv->lv1,lv->rows,lv->columns);
+
+    char direction;
 
     do{
-        printf("\nWhich direction ? : ");
-        scanf("%c", &direction);
-        fflush(stdin);
-        printf("Direction : %c\n", direction);
+        printMap(lv->lv1,lv->rows,lv->columns);
 
-    }while(direction != 'p');*/
+        printf("\nWhich direction ? (z : up, s : down, q : left, d : right ): ");
+        fflush(stdin);
+        scanf("%c", &direction);
+
+        move(lv->lv1, pc, direction);
+    }while(direction != 'e');
 }
