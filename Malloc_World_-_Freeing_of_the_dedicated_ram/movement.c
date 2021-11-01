f8fc2f144f8b91e6a4333c36b8375cdb4830bccb
Pierre <bonnamy.77@gmail.com>
2021-11-01 18:01:53 +0100

    fix

--- Malloc_World_-_Freeing_of_the_dedicated_ram/movement.c
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/movement.c
@@ -6,24 +6,20 @@ void move (int ** level, PlayerPos * pc, char direction){
     level[pc->posRow][pc->posColumn] = 0;
 
     if(direction == 'z'){
-        level[(pc->posRow)-1][pc->posColumn] = 1;
         pc->posRow -= 1;
     }else if(direction == 's'){
-        level[pc->posRow+1][pc->posColumn] = 1;
         pc->posRow += 1;
     }else if(direction == 'q'){
-        level[pc->posRow][pc->posColumn-1] = 1;
         pc->posColumn -= 1;
     }else if (direction == 'd'){
-        level[pc->posRow][pc->posColumn+1] = 1;
         pc->posColumn += 1;
     }
+
+    level[pc->posRow][pc->posColumn] = 1;
 
 }
 
 void handleMovement(Levels *lv, PlayerPos *pc){
-
-    //printMap(lv->lv1,lv->rows,lv->columns);
 
     char direction;
 
