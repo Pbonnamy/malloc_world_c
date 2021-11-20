d181ce528f063d17913fa86c46646070c742979f
Pierre <bonnamy.77@gmail.com>
2021-11-20 11:45:58 +0100

    more combat

--- Malloc_World_-_Freeing_of_the_dedicated_ram/headers/header.h
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/headers/header.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 
 typedef struct MonsterNode MonsterNode;
 typedef struct Levels Levels;
--- Malloc_World_-_Freeing_of_the_dedicated_ram/sources/combat.c
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/sources/combat.c
@@ -2,10 +2,10 @@
 
 int flee(){
     int rate = rand2(1,100);
-    int loop = 0;
+    int loop = 1;
 
     if(rate <= FLEE_RATE){
-        loop = 1;
+        loop = 0;
     }
 
     if(loop == 1){
@@ -97,22 +97,34 @@ InventoryNode *itemSelect(InventoryNode *inventoryHead, int itemType){
 
 void attack(MonsterNode *monsterNode, InventoryNode *weapon){
     if(weapon != NULL && weapon->durability > 0){
-        int damage = atoi(ITEMS[monsterNode->reference][_info]);
+        int damage = atoi(ITEMS[weapon->reference][_info]);
         monsterNode->hp -= damage;
 
         if(monsterNode->hp <= 0){
             monsterNode->hp = 0;
         }
 
-        printf("You dealt %d damage\n", damage);
+        printf("You deal %d damage\n", damage);
         weapon->durability -= WEAR_COMBAT;
 
         if(weapon->durability == 0){
             printf("Your weapon is now broken\n");
         }
     }else{
-        printf("Your dealt 0 damage\n");
+        printf("Your deal 0 damage\n");
     }
+}
+
+void monsterAttack(Player *player, InventoryNode *armor, MonsterNode *monster){
+    int damage = atoi(MONSTERS[monster->reference][_monsterDamage]);
+
+    if(armor != NULL){
+        damage -= round((damage*atoi(ITEMS[armor->reference][_info]))/100);
+    }
+
+    player->currentHp -= damage;
+
+    printf("You take %d damage\n", damage);
 }
 
 int handleCombat(MonsterNode *monsterNode, Player *player){
@@ -120,14 +132,14 @@ int handleCombat(MonsterNode *monsterNode, Player *player){
     int loop = 1;
     int defeated = 0;
 
-    printf("\nYour are fighting a %s - Hp : %d | Dmg : %s\n\n", MONSTERS[monsterNode->reference][_name], monsterNode->hp, MONSTERS[monsterNode->reference][_monsterDamage]);
+    printf("\nYour are fighting a %s - Hp : %d | Dmg : %s\n", MONSTERS[monsterNode->reference][_name], monsterNode->hp, MONSTERS[monsterNode->reference][_monsterDamage]);
 
     InventoryNode *weapon = itemSelect(player->inventory, _weapon);
 
     InventoryNode *armor = itemSelect(player->inventory, _armor);
 
     do{
-        printf("Choose an action ? (a : attack, p : use a potion, f : try to flee)  : ");
+        printf("\nChoose an action ? (a : attack, p : use a potion, f : try to flee)  : ");
         fflush(stdin);
         scanf("%c", &action);
 
@@ -140,6 +152,22 @@ int handleCombat(MonsterNode *monsterNode, Player *player){
         }else if(action == 'f'){
             loop = flee();
         }
+
+        if(loop){
+            if(monsterNode->hp > 0){
+                monsterAttack(player, armor, monsterNode);
+
+                printf("Your HP : %d | Ennemy HP : %d", player->currentHp, monsterNode->hp);
+            }else if(player->currentHp > 0){
+
+                loop = 0;
+                defeated = 1;
+            }else{
+                printf("You are dead. GAME OVER.");
+                loop = 0;
+            }
+        }
+
 
     }while(loop);
 
