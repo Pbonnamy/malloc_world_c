12daf25708ec89caa21cfe02bbfe18957bfe2197
Pierre <bonnamy.77@gmail.com>
2021-11-22 22:51:03 +0100

    main menu

--- Malloc_World_-_Freeing_of_the_dedicated_ram/sources/menu.c
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/sources/menu.c
@@ -9,7 +9,6 @@ void handleAction(Levels *levels, Player *player, char action){
     }else if(action == '3'){
         printf("\nTODO : Save\n\n");
     }
-
 }
 
 void gameLoop(Levels *levels, Player *player){
@@ -35,4 +34,23 @@ void gameLoop(Levels *levels, Player *player){
         handleAction(levels, player, action);
 
     }while(action != '4');
+}
+
+void handleMainMenu(char action){
+
+    if(action == '1'){
+        Levels *levels = malloc(sizeof(Levels));
+        Player *player = malloc(sizeof(Player));
+
+        initMap(levels, 10, 10, player);
+        initPlayer(player);
+
+        //printRessourceList(levels->lv1->ressourceList);
+        //printMonsterList(levels->lv1->monsterList);
+
+        gameLoop(levels, player);
+    }else if(action == '2'){
+        printf("\nTODO : Load Save\n\n");
+    }
+
 }
