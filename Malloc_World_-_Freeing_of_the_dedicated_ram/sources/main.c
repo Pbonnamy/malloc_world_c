1e3b480cfaaf0c6e496255d607ef1ca5677149c3
sbouadi <sarahbouadi0508@gmail.com>
2021-11-22 23:11:33 +0100

    better main

--- Malloc_World_-_Freeing_of_the_dedicated_ram/sources/main.c
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/sources/main.c
@@ -15,5 +15,18 @@ int main()
 
     gameLoop(levels, player);
 
+    int** map = malloc(sizeof(int*)*4);
+    for (size_t i =0; i <4; i+=1) {
+        map[i] = malloc(sizeof(int)*4);
+    }
+    for (size_t i = 0; i<4; i+=1) {
+        for (size_t j = 0; j<4; j+=1) {
+            map[i][j] = 0;
+        }
+    }
+    writeInFile(map);
+    readFile();
+    freeMap(map);
+
     return 0;
 }
