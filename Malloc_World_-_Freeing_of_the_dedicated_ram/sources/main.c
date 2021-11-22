3f1eebc0b18407a81c70636872d5494bc7117e97
Pierre <bonnamy.77@gmail.com>
2021-11-22 23:29:30 +0100

    removed problem code

--- Malloc_World_-_Freeing_of_the_dedicated_ram/sources/main.c
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/sources/main.c
@@ -23,18 +23,5 @@ int main(){
 
     }while(action != '3');
 
-    int** map = malloc(sizeof(int*)*4);
-    for (size_t i =0; i <4; i+=1) {
-        map[i] = malloc(sizeof(int)*4);
-    }
-    for (size_t i = 0; i<4; i+=1) {
-        for (size_t j = 0; j<4; j+=1) {
-            map[i][j] = 0;
-        }
-    }
-    writeInFile(map);
-    readFile();
-    freeMap(map);
-
     return 0;
 }
